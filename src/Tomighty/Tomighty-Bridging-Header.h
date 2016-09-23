cb6f4a39dec8776dfc1108060d1ff3dfbf07cea2
Devon Tucker <dtucker@boundlessgeo.com>
2016-09-23 16:36:51 -0700

    Starting Swift conversion

---
+++ src/Tomighty/Tomighty-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "TYTimerContext.h"
+#import "TYSoundPlayer.h"
+#import "TYPreferences.h"
+#import "TYEventBus.h"
