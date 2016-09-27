898a7c4e60988746e3085e0ac84070ac686626b4
Devon Tucker <dtucker@boundlessgeo.com>
2016-09-26 23:37:29 -0700

    Interim

---
+++ src/TomightyTests/SystemTimerTest.swift
@@ -0,0 +1,27 @@
+//
+//  Tomighty - http://www.tomighty.org
+//
+//  This software is licensed under the Apache License Version 2.0:
+//  http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+
+
+import XCTest
+import Tomighty
+
+class SystemTimerTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInterrupt() {
+      XCTAssert(false)
+    }
+}
